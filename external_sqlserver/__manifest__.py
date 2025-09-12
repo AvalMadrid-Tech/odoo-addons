bc2fc851a2c0aac0c0c88f9ce7db8ed2dc5e4b57
GSR80 <guillermo.santamaria@avalmadrid.es>
2025-09-12 12:54:32 +0200

    create views  and security

--- external_sqlserver/__manifest__.py
+++ external_sqlserver/__manifest__.py
@@ -1,13 +1,22 @@
 {
-    'name': 'External SQL Server Connector',
-    'version': '1.0',
-    'summary': 'Módulo base para conexión a SQL Server',
-    'author': 'Guillermo',
-    'depends': ['base'],
+    "name": "External SQL Server Connector",
+    "version": "18.0.1.0",
+    "author": "Guillermo",
+    "license": "LGPL-3",
+    "category": "Tools",
+    "summary": "Conector genérico para SQL Server",
+    "description": """
+Módulo base para conexión a SQL Server desde Odoo.
+Permite probar la conexión y ejecutar consultas.
+    """,
+    "depends": ["base"],
     'data': [
+        'security/ir.model.access.csv',
+        'security/sqlserver_security.xml',
+        'views/res_config_settings_views.xml',
         'views/test_connection_wizard_views.xml',
     ],
-    'installable': True,
-    'application': False,  
-}
 
+    "application": False,
+    "installable": True,
+}
